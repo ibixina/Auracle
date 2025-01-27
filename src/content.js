968e7f9f95b1abafdd65a07a739c288a77449d7d
Shishir <theshishir01@gmail.com>
2025-01-27 15:19:48 -0600

    updates

--- src/content.js
+++ src/content.js
@@ -26,7 +26,7 @@ const genAI = new GoogleGenerativeAI(apiKey);
 const model = genAI.getGenerativeModel({
   model: "gemini-1.5-flash-8b",
   systemInstruction:
-    "be concise and accurate. do not hallucinate. verify everything you say. you will be given texts and your job is to summarize them in a digestible manner. depending on the subject, give proper examples and analogies to make it easier to understand. only say what you are fully confident about. do not exlcude any vital information. explain any unfamiliar ideas or terms. explain not just what but also the how.",
+    "be concise and accurate. do not hallucinate. verify everything you say. you will be given texts and your job is to summarize them in a digestible manner. depending on the subject, give proper examples and analogies to make it easier to understand. only say what you are fully confident about. do not exlcude any vital information. explain any unfamiliar ideas or terms. explain not just what but also the how. the answer should be short and concise.",
 });
 
 const generationConfig = {
@@ -37,13 +37,39 @@ const generationConfig = {
   responseMimeType: "text/plain",
 };
 
-async function run(message) {
-  const chatSession = model.startChat({
-    generationConfig,
-    history: [],
-  });
+const chatSession = model.startChat({
+  generationConfig,
+  history: [],
+});
 
+async function run(message) {
   const result = await chatSession.sendMessage(message);
-  alert(result.response.text());
   console.log(result.response.text());
+  answerContainer.innerText = result.response
+    .text()
+    .replaceAll("`", "")
+    .replace("html", "");
+  answerContainer.style.display = "block";
 }
+
+const answerContainer = document.createElement("div");
+answerContainer.id = "answerSumm";
+answerContainer.style.display = "none";
+answerContainer.style.position = "fixed";
+answerContainer.style.bottom = "0";
+answerContainer.style.right = "0";
+answerContainer.style.backgroundColor = "#000";
+answerContainer.style.opacity = "0.8";
+answerContainer.style.color = "#fff";
+answerContainer.style.padding = "10px";
+answerContainer.style.zIndex = "99999999";
+answerContainer.style.borderRadius = "5px";
+answerContainer.style.maxHeight = "33%";
+answerContainer.style.overflow = "scroll";
+document.body.appendChild(answerContainer);
+
+document.addEventListener("click", (e) => {
+  if (e.target.id === "answerSumm") {
+    answerContainer.style.display = "none";
+  }
+});
