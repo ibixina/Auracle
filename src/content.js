193a1d0d4f8b5b82e9887a99818a380abe2eee47
Shishir <theshishir01@gmail.com>
2025-01-27 14:54:32 -0600

    removed history

--- src/content.js
+++ src/content.js
@@ -14,8 +14,6 @@ function execute(text) {
   run(text);
 }
 
-const history = [];
-
 const {
   GoogleGenerativeAI,
   HarmCategory,
@@ -26,7 +24,7 @@ const apiKey = API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash",
+  model: "gemini-1.5-flash-8b",
   systemInstruction:
     "be concise and accurate. do not hallucinate. verify everything you say. you will be given texts and your job is to summarize them in a digestible manner. depending on the subject, give proper examples and analogies to make it easier to understand. only say what you are fully confident about. do not exlcude any vital information. explain any unfamiliar ideas or terms. explain not just what but also the how.",
 });
@@ -42,11 +40,10 @@ const generationConfig = {
 async function run(message) {
   const chatSession = model.startChat({
     generationConfig,
-    history: history,
+    history: [],
   });
 
   const result = await chatSession.sendMessage(message);
   alert(result.response.text());
   console.log(result.response.text());
-  history.push(result.response.text());
 }
